ee2e1fb8a480cbc52efdc53a1a5fd8d53b49d709
WebDevDaniels <adrian-schu@gmx.de>
2025-05-16 15:52:28 +0000

    routing and first components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Startseite',
+        component: LoginComponent
+    },
+    {
+        path: '**',
+        title: 'Ups!',
+        component: NotFoundComponent
+    }
+];
